d7c6ab41d4849664607f8d60e7f77c1db1feb0c8
ArenDeveloper05 <antisystem005@mail.ru>
2023-08-11 14:26:23 +0400

    scss bem hierarchy

--- src/components/YourCodeHere/YourCodeHere.jsx
+++ src/components/YourCodeHere/YourCodeHere.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { UserContainer } from '../User/UserContainer.jsx';
-import './YourCodeHere.scss';
+import { UIDropdown } from '../UI/UIDropdown.jsx';
 //Icons
 import modalCloseIcon from "../../assets/images/icon-close.svg"
 import OP_LOGO from "../../assets/images/img-op.svg"
 import Add_Url_Icon from "../../assets/images/icon-add-url.svg"
 import Choose_File_Icon from "../../assets/images/icon-choose-file.svg"
 import Icon__File from "../../assets/images/icon-file.svg"
-import { UIDropdown } from '../UI/UIDropdown.jsx';
+//SCSS
+import './YourCodeHere.scss';
 
 
 
@@ -17,28 +18,35 @@ export function YourCodeHere(){
   return (
     <UserContainer>
 
-      <div className='Report__Background '>
-        <div className='Report__Modal'>
+      <div className='Report__background '>
+        <div className='Report__modal'>
 
 
-          <div className='Report__Modal-container'>
-            <div className='Report__Modal-container__Header'>
+          <div className='Report__modal__container'>
+            <div className='Report__modal__container__header'>
                 <p>Report submission</p>
-                <div className='Report__Modal-container__Header-content'>
+                <div className='Report__modal__container__header__content'>
 
-                    <div className='Report__Modal-container__Header-content__Description'>
-                        <div className='Logo'>
+                    <div className='Report__modal__container__header__content__description'>
+                        <div className='Report__modal__container__header__content__description__logo'>
                           <img src={OP_LOGO} alt="logo" />
                         </div>
 
-                        <div className='Info'>
-                          <h4>Optimism</h4>
-                          <div className='Info-desc'><p>An open stack builders can create social</p></div>  {/* На случай, если текст слишком большой */}
-                          <p><span>Type</span> Smart Contract</p>
+                        <div className='Report__modal__container__header__content__description__info'>
+                          <h4>Optimism Lorem ipsum dolor sit 
+                            amet consectetur adipisicing elit.
+                            Doloribus molestiae distinctio quibusdam minus ducimus excepturi
+                            voluptatum fugiat quae. Ad, commodi totam distinctio fugit nulla 
+                            sit vero nam saepe minus facere.</h4> {/* На случай, если текст слишком большой */}
+                          <div className='Report__modal__container__header__content__description__info__desc'><p>An open stack builders can create social</p></div>  {/* На случай, если текст слишком большой */}
+                          <p>
+                            <span>Type</span>
+                             Smart Contract
+                          </p>
                         </div>
                     </div>
 
-                    <div className='Report__Modal-container__Header-content__Buttons'>
+                    <div className='Report__modal__container__header__content__buttons'>
                       <button className="UIButton UIButton--green-outline">Save as draft</button>
                       <button className="UIButton UIButton--green">Submit Report</button>
                     </div>
@@ -47,48 +55,48 @@ export function YourCodeHere(){
             </div>
 
 
-            <div className='Report__Modal-container__Main'>
-                <div className='Report__Modal-container__Main-fields'>
+            <div className='Report__modal__container__main'>
+                <div className='Report__modal__container__main__fields'>
 
-                    <div className='Report__Modal-container__Main-fields-field'>
+                    <div className='Report__modal__container__main__fields__field'>
                         <p>Asset</p>
                         <UIDropdown className="my-custom-class" title="Select assets in scope" items={[ {title: "Item 1"}, {title: "Item 2"} ]}></UIDropdown>
                     </div>
 
-                    <div className='Report__Modal-container__Main-fields-field     Field__error'>
+                    <div className='Report__modal__container__main__fields__field     Field__error'>
                         <p>Severity</p>
                         <UIDropdown className="my-custom-class" title="Select Severity" items={[ {title: "Item 1"}, {title: "Item 2"} ]}></UIDropdown>
                         <p>Please select a sevrity of the potential vulnerability</p>
                     </div>
 
-                    <div className='Report__Modal-container__Main-fields-field    Report__title__input     Field__error'>
+                    <div className='Report__modal__container__main__fields__field    Report__title__input     Field__error'>
                         <p><span>Report Title</span> <span>12</span></p>
                         <input className="UIInput" type="text" placeholder="Placeholder..."/>
                         <p>Please enter your report title</p>
                     </div>
 
-                    <div className='Report__Modal-container__Main-fields-field     Report__content__field      Field__error '>
+                    <div className='Report__modal__container__main__fields__field     Report__content__field      Field__error '>
                         <p><span>Report Content</span> <span>15,000</span></p>
-                        <div className='Report__Content__Area'>
+                        <div className='Report__content__area'>
 
-                          <div className='Report__Content__Area__Header'>
+                          <div className='Report__content__area__header'>
 
-                              <div className='Report__Content__Area__Header-modes'>
-                                <div className='Report__Content__Area__Header-modes-mode    Active--Markdown--Mode'>
+                              <div className='Report__content__area__header__modes'>
+                                <div className='Report__content__area__header__modes__mode    Active--Markdown--Mode'>
                                   <span>Write</span>
                                 </div>
-                                <div className='Report__Content__Area__Header-modes-mode '>
+                                <div className='Report__content__area__header__modes__mode '>
                                   <span>Preview</span>
                                 </div>
                               </div>
 
                               
-                              <div className='Report__Content__Area__Header-info'>
+                              <div className='Report__content__area__header__info'>
                                 <span>How to use markdown</span>
                               </div>
 
                           </div>
-                          <div className='Report__Content__Area__Content'>
+                          <div className='Report__content__area__content'>
 
                           </div>
 
@@ -97,60 +105,64 @@ export function YourCodeHere(){
                         <p>You have reached the character limit</p>
                     </div>
 
-                    <div className='Report__Modal-container__Main-fields-field Additional-links '>
-                      <div className='Additional-links__Title'>
-                          <p>Additional links <span>(optional)</span></p>
+                    <div className='Report__modal__container__main__fields__field AdditionalLinks '>
+                      <div className='AdditionalLinks__title'>
+                          <p>
+                            Additional links
+                            <span>(optional)</span>
+                          </p>
                       </div>
 
-                      <div className='Additional-links__Box'>
-                          <div className='Additional-links__Box-item'>
+                      <div className='AdditionalLinks__box'>
+                          <div className='AdditionalLinks__box__item'>
                               <input className="UIInput" type="text" placeholder="http://         Enter an additional link" />
                           </div>
-                          <div className='Additional-links__Box-item    Additional--Link--Error'>
+                          <div className='AdditionalLinks__box__item    Additional--Link--Error'>
                               <input className="UIInput" type="text" placeholder="http://         Enter an additional link"/>
                               <p className='Additional--Link--Error--message'>Please enter a valid URL</p>
                           </div>
-                          <div className='Additional-links__Box-item'>
+                          <div className='AdditionalLinks__box__item'>
                               <input className="UIInput" type="text" placeholder="http://         Enter an additional link"/>
                           </div>
-                          <div className='Additional-links__Box-item'>
+                          <div className='AdditionalLinks__box__item'>
                               <input className="UIInput" type="text" placeholder="http://         Enter an additional link"/>
                               
                           </div>
 
                       </div>
 
-                      <div className='Additional-links__AddUrl'>
+                      <div className='AdditionalLinks__addUrl'>
                         <img src={Add_Url_Icon} alt="add-url-icon" />
                         <span>Add another URL</span>
                       </div>
 
                     </div> 
-                    <div className='Report__Modal-container__Main-fields-field Attachments '>
+                    <div className='Report__modal__container__main__fields__field Attachments '>
 
-                      <div className='Attachments__Title'>
+                      <div className='Attachments__title'>
                         <p>Attachments  <span>(optional)</span></p>
                       </div>
 
-                      <div className='Attachments__ChooseFile'>
-                        <div className='Attachments__ChooseFile__Icon'>
+                      <div className='Attachments__chooseFile'>
+                        <div className='Attachments__chooseFile__icon'>
                           <img src={Choose_File_Icon} alt="choose-file-icon" />
                         </div>
                         <p>Drag and drop files here or click to browse</p>
                       </div>
                       
-                      <div className='Attachments__Files'>
+                      <div className='Attachments__files'>
 
-                          <div className='Attachments__Files__File'>
+                          <div className='Attachments__files__file'>
 
-                            <div className='Attachments__Files__File__Name'>
-                                <div className='Attachments__Files__File__Name__Icon'>
+                            <div className='Attachments__files__file__name'>
+                                <div className='Attachments__files__file__name__icon'>
                                     <img src={Icon__File} alt="attachment-file-name-icon" />
                                 </div>
                                 <p>F1 screenshot.png</p>  {/* На случай, если текст слишком большой */}
                             </div>
 
-                            <div className='Attachments__Files__File__Close'>
+                            <div className='Attachments__files__file__close'>
+
                               <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path fill-rule="evenodd" clip-rule="evenodd" d="M17.9998 6.94376L17.056 6L11.9996 11.0562L6.94397 6.00071L6.00016 6.94447L11.0558 11.9999L6 17.0555L6.94381 17.9993L11.9996 12.9437L17.0562 18L18 17.0562L12.9435 11.9999L17.9998 6.94376Z" fill="#F5F5F5"/>
                               </svg>
@@ -158,17 +170,17 @@ export function YourCodeHere(){
 
                           </div>
 
-                          <div className='Attachments__Files__File'>
+                          <div className='Attachments__files__file'>
 
-                            <div className='Attachments__Files__File__Name'>
-                                <div className='Attachments__Files__File__Name__Icon'>
+                            <div className='Attachments__files__file__name'>
+                                <div className='Attachments__files__file__name__icon'>
                                     <img src={Icon__File} alt="attachment-file-name-icon" />
                                 </div>
                                 <p>F1 screenshot.png</p>
                             </div>
 
 
-                            <div className='Attachments__Files__File__Close'>
+                            <div className='Attachments__files__file__close'>
                               <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path fill-rule="evenodd" clip-rule="evenodd" d="M17.9998 6.94376L17.056 6L11.9996 11.0562L6.94397 6.00071L6.00016 6.94447L11.0558 11.9999L6 17.0555L6.94381 17.9993L11.9996 12.9437L17.0562 18L18 17.0562L12.9435 11.9999L17.9998 6.94376Z" fill="#F5F5F5"/>
                               </svg>
@@ -181,7 +193,7 @@ export function YourCodeHere(){
                     </div>
 
                 </div>
-                <div className='Report__Modal-container__Main-buttons'>
+                <div className='Report__modal__container__main-buttons'>
                     <button className="UIButton UIButton--green-outline">Save as draft</button>
                     <button className="UIButton UIButton--green">Submit Report</button>
                 </div>
@@ -191,8 +203,8 @@ export function YourCodeHere(){
           </div>
 
 
-          <div className='Report__Modal-close'>
-            <div className='Report__Modal-close-img'>
+          <div className='Report__modal-close'>
+            <div className='Report__modal-close-img'>
               <img src={modalCloseIcon} alt="modalCloseIcon" />
             </div>
           </div>
